7a0fd1737631413b38bb4e874e96808a193db941
VMIGERJO <jordi.vm@live.be>
2023-12-10 17:31:52 +0100

    Implements Prescription mapping tests as well as creating EntityFactory and DTO factory to generate test data

---
+++ TestProject/Utilities/DTOFactory.cs
@@ -0,0 +1,101 @@
+﻿using DAL.Entities;
+using BL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Utilities
+{
+    public class DTOFactory
+    {
+        public DoctorDTO CreateDoctorDTO()
+        {
+            return new DoctorDTO
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Specialization = "Cardiologist"
+            };
+        }
+
+        public void EstablishDoctorPrescriptionDTORelationship(DoctorDTO doctor, PrescriptionDTO prescription)
+        {
+            // Establish a relationship between Doctor and Prescription
+            new List<PrescriptionDTO> { prescription };
+        }
+
+        public PatientDTO CreatePatientDTO()
+        {
+            var addressDTO = CreateAddressDTO();
+
+            return new PatientDTO
+            {
+                FirstName = "Alice",
+                LastName = "Smith",
+                Age = 30,
+                MedicalHistory = "No significant medical history",
+                Address = addressDTO
+            };
+        }
+
+        public AddressDTO CreateAddressDTO()
+        {
+            return new AddressDTO
+            {
+                Street = "123 Main Street",
+                HouseNumber = "42",
+                Appartment = "Apt 301",
+                City = "Cityville",
+                PostalCode = "12345",
+                Country = "Countryland"
+            };
+        }
+
+        public void EstablishPatientPrescriptionDTORelationship(PatientDTO patient, PrescriptionDTO prescription)
+        {
+            // Establish a relationship between Patient and Prescription
+            patient.Prescriptions = new List<PrescriptionDTO> { prescription };
+        }
+
+        public MedicationDTO CreateMedicationDTO()
+        {
+            return new MedicationDTO
+            {
+                ActiveSubstance = "Active Substance",
+                Name = "MedicineX",
+                Dosage = "10mg",
+                Manufacturer = "PharmaCorp"
+            };
+        }
+
+        public void EstablishPrescriptionMedicationDTORelationship(PrescriptionDTO prescription, MedicationDTO medication)
+        {
+            // Establish a relationship between Prescription and Medication
+            prescription.Medications.Add(medication);
+        }
+
+        public PrescriptionDTO CreatePrescriptionDTO(DoctorDTO doctor, PatientDTO patient)
+        {
+            return new PrescriptionDTO
+            {
+                DoctorId = doctor.Id,
+                PatientId = patient.Id,
+                PrescriptionDate = DateTime.Now,
+            };
+        }
+
+        public PrescriptionDTO CreatePopulatedPrescriptionDTO()
+        {
+            var doctorDTO = CreateDoctorDTO();
+            var patientDTO = CreatePatientDTO();
+            var medicationDTO = CreateMedicationDTO();
+            var prescriptionDTO = CreatePrescriptionDTO(doctorDTO, patientDTO);
+            EstablishPrescriptionMedicationDTORelationship(prescriptionDTO, medicationDTO);
+            EstablishPatientPrescriptionDTORelationship(patientDTO, prescriptionDTO);
+            EstablishDoctorPrescriptionDTORelationship(doctorDTO, prescriptionDTO);
+            return prescriptionDTO;
+        }
+    }
+}
---
+++ TestProject/Utilities/EntityFactory.cs
@@ -0,0 +1,98 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace TestProject.Utilities {
+public class EntityFactory
+{
+    public Doctor CreateDoctor()
+    {
+        return new Doctor
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Specialization = "Cardiologist"
+        };
+    }
+
+    public void EstablishDoctorPrescriptionRelationship(Doctor doctor, Prescription prescription)
+    {
+            // Establish a relationship between Doctor and Prescription
+            new List<Prescription> { prescription };
+        }
+
+    public Patient CreatePatient()
+    {
+        var address = CreateAddress();
+
+        return new Patient
+        {
+            FirstName = "Alice",
+            LastName = "Smith",
+            Age = 30,
+            MedicalHistory = "No significant medical history",
+            Address = address
+        };
+    }
+
+    public Address CreateAddress()
+    {
+        return new Address
+        {
+            Street = "123 Main Street",
+            HouseNumber = "42",
+            Appartment = "Apt 301",
+            City = "Cityville",
+            PostalCode = "12345",
+            Country = "Countryland"
+        };
+    }
+
+    public void EstablishPatientPrescriptionRelationship(Patient patient, Prescription prescription)
+    {
+        // Establish a relationship between Patient and Prescription
+        patient.Prescriptions = new List<Prescription> { prescription };
+    }
+
+    public Medication CreateMedication()
+    {
+        return new Medication
+        {
+            ActiveSubstance = "Active Substance",
+            Name = "MedicineX",
+            Dosage = "10mg",
+            Manufacturer = "PharmaCorp"
+        };
+    }
+
+    public void EstablishPrescriptionMedicationRelationship(Prescription prescription, Medication medication)
+    {
+        // Establish a relationship between Prescription and Medication
+        prescription.Medications.Add(medication);
+    }
+
+    public Prescription CreatePrescription(Doctor doctor, Patient patient)
+    {
+        return new Prescription
+        {
+            Patient = patient,
+            Doctor = doctor,
+            DoctorId = doctor.Id,
+            PatientId = patient.Id,
+            PrescriptionDate = DateTime.Now,
+        };
+    }
+    
+    public Prescription CreatePopulatedPrescription()
+        {
+            var doctor = CreateDoctor();
+            var patient = CreatePatient();
+            var medication = CreateMedication();
+            var prescription = CreatePrescription(doctor, patient);
+            EstablishPrescriptionMedicationRelationship(prescription, medication);
+            EstablishPatientPrescriptionRelationship(patient, prescription);
+            EstablishDoctorPrescriptionRelationship(doctor, prescription);
+            return prescription;
+        }
+  }
+}
